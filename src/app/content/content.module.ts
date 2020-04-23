c334721daaa5764e0ac3193b7ca3c10eda22d077
Adrian Sowiński <theaner123@gmail.com>
2020-04-23 15:58:38 +0200

    Added basic countdown

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -1,18 +1,20 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home/home.component';
-import { SpeakersComponent } from './speakers/speakers.component';
-import { VenueComponent } from './venue/venue.component';
-import { ScheduleComponent } from './schedule/schedule.component';
+import {SpeakersComponent} from './speakers/speakers.component';
+import {VenueComponent} from './venue/venue.component';
+import {ScheduleComponent} from './schedule/schedule.component';
 import {ButtonsModule} from 'ngx-bootstrap/buttons';
-import { RegisterComponent } from './register/register.component';
+import {RegisterComponent} from './register/register.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [HomeComponent, SpeakersComponent, VenueComponent, ScheduleComponent, RegisterComponent],
   imports: [
     CommonModule,
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    RouterModule
   ]
 })
 export class ContentModule { }
--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  countdown: TimerDate;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.countdown = this.getTimeRemaining(new Date('Apr 26, 2020'));
+    this.initializeClock(new Date('Apr 26, 2020'));
   }
 
+  initializeClock(endtime){
+    const timeinterval = setInterval(() => {
+      this.countdown = this.getTimeRemaining(endtime);
+      if (this.countdown.total <= 0){
+        clearInterval(timeinterval);
+      }
+    }, 1000);
+  }
+
+  getTimeRemaining(endtime): TimerDate{
+    const t = Date.parse(endtime) - Date.parse(new Date().toString());
+    const secondsN = Math.floor( (t / 1000) % 60 );
+    const seconds = secondsN > 9 ? '' + secondsN : '0' + secondsN;
+    const minutesN = Math.floor( (t / 1000 / 60) % 60 );
+    const minutes = minutesN > 9 ? '' + minutesN : '0' + minutesN;
+    const hoursN = Math.floor( (t / (1000 * 60 * 60)) % 24 );
+    const hours = hoursN > 9 ? '' + hoursN : '0' + hoursN;
+    const daysN = Math.floor( t / (1000 * 60 * 60 * 24) );
+    const days = daysN > 9 ? '' + daysN : '0' + daysN;
+
+    return {
+      total: t,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+}
+
+interface TimerDate {
+  total: number;
+  days: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
 }
